aa87657ebe763c3680e83d38a1a113b0b7342661
pujamalca <pujamalca09@gmail.com>
2025-09-03 11:07:18 +0700

    update : build and run

--- config/database.go
+++ config/database.go
@@ -1,24 +1,24 @@
 package config
 
-import {
+import (
 	"fmt"
 	"time"
 	"api-contact-form/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
-}
+)
 
 var DB *gorm.DB
 
 func InitDB(){
-	dbUser := GetEnv("DB_USER", "user")
-	dbPassword := GetEnv("DB_PASSWORD", "password")
-	dbHost := GetEnv("DB_HOST", "db")
+	dbUser := GetEnv("DB_USER", "alca")
+	dbPassword := GetEnv("DB_PASSWORD", "alca")
+	dbHost := GetEnv("DB_HOST", "localhost")
 	dbPort := GetEnv("DB_PORT", "3306")
 	dbName := GetEnv("DB_NAME", "contactsdb")
 
-	dsn =: fmt.Sprintf("%s:%s@tcp(%s:%s)/s?charset=uf8mb4&parseTime=True$loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	var err error
 
@@ -38,7 +38,7 @@ func InitDB(){
 
 	sqlDB.SetMaxOpenConns(10)
 	sqlDB.SetMaxIdleConns(5)
-	sqlDB.SetConnMaxLifetime(time:hour)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	if err := DB.AutoMigrate(&models.Contact{}); err != nil{
 		panic(fmt.Sprintf("AutoMigrate Failed: %v", err))
